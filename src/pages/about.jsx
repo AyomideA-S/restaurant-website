78febe5de42683c1b130f6c3f3a1ae5c0fce5ef2
Ernesto <66752274+Ernest2026@users.noreply.github.com>
2022-04-28 00:40:33 +0100

    Updated about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 const About = () =>{
   return (
-    <div className='content'>
-      <h3>About Us</h3>
+    <div class="container mt-5">
+<h1>About page</h1>
+  <div class="row">
+    
+    <div class="col">
+      <h2>Vision</h2>
+      <h5>Lorem ipsum dolor sit ameh</h5>
+      <div class="fakeimg">Starter Kit</div>
+      <p>Lorem ipsum..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+
+      <h2 class="mt-5">Mission</h2>
+      <h5>Lorem ipsum dolor sit ameh</h5>
+      <div class="fakeimg">Starter Kit</div>
+      <p>Lorem ipsum..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
     </div>
+  </div>
+</div>
+
   );
 };
 
