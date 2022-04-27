eaf9a76760d7c88c4d84fdb5cba96f13ba97679b
Ayomide Ayodele-Soyebo <midesuperbest@gmail.com>
2022-04-27 21:03:25 +0100

    Styled Products page

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 const Item = ({name, price, imgSrc}) => {
     return (
-        <div className="card" style={{width: "100%"}}>
-            <img className="card-img-top" src={imgSrc} alt="Meal" />
-            <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <p className="card-text">{price}</p>
-                <div className='d-flex justify-content-between'>
-                    <button className="btn btn-outline-primary" style={{width: "48%"}}>
-                        Units: 1
-                    </button>
-                    <button className="btn btn-primary" style={{width: "48%"}}>
-                        Add to cart
-                    </button>
+        <div className="showcase-to section4-to" style={{width: "25%"}}>
+            <div className="container-to box-flex-to">
+                <div className="box-to">
+                    <img className="card-img-top" src={imgSrc} alt="Meal" />
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">{price}</p>
+                    <div className="flex-gerne-to">
+                        <button className="btn btn-outline-primary" style={{width: "48%"}}>
+                            Units: 1
+                        </button>
+                        <button className="btn btn-primary" style={{width: "48%"}}>
+                            Add to cart
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -8,21 +8,27 @@ import Shawarma from '../images/shawarma.webp'
 const Product = () =>{
   return (
     <div>
-      <h3>Products</h3>
-      <Item 
-      imgSrc={Pizza}
-      name="Pizza"
-      price="#6,000"/><Item 
-      imgSrc={Pie}
-      name="Pie"
-      price="#2,500"/><Item 
-      imgSrc={Burger}
-      name="Burger"
-      price="#2,000"/><Item 
-      imgSrc={Shawarma}
-      name="Shawarma"
-      price="#1,500"/>
+      <div className="answer-section-header-to">
+        <h1>Products</h1>
+      </div>
+
+      <div className='box-flex-to container-to'>
+        <Item 
+        imgSrc={Pizza}
+        name="Pizza"
+        price="#6,000"/><Item 
+        imgSrc={Pie}
+        name="Pie"
+        price="#2,500"/><Item 
+        imgSrc={Burger}
+        name="Burger"
+        price="#2,000"/><Item 
+        imgSrc={Shawarma}
+        name="Shawarma"
+        price="#1,500"/>
+      </div>
     </div>
+    
   );
 };
 
